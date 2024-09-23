293170b12235d1c5a77ff2a9c7e305b9eeb48d0f
unknown <xcesaryt@gmail.com>
2024-09-23 07:10:52 -0300

    Testando o código sem o SQLlite, pq parece que o AndroidStudio está com um erro chamado HERMES

--- App.js
+++ App.js
@@ -2,8 +2,8 @@ import React, { useState, useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import TelaInicial from './src/TelaInicial';
-import TelaAgendamentos from './src/TelaAgendamento';
+import TelaInicial from './Telas/TelaInicial';
+import TelaAgendamentos from './Telas/TelaAgendamento';
 
 const Stack = createStackNavigator();
 
